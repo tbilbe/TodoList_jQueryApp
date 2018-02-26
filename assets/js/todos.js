f534eba5099bdb494d74f826e144845550aad4f8
Tom Bilbe <tbilbe@gmail.com>
2018-02-26 20:11:57 +0000

    functionality completed - dummy data inside html

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,24 @@
-alert('connected!')
+//check off specific todos by clicking
+
+$('ul').on('click', 'li', function() {
+  $(this).toggleClass('completed');
+});
+
+//click on X to delete todo
+$('ul').on('click', 'span', function(e) {
+  $(this)
+    .parent()
+    .fadeOut('slow', function() {
+      $(this).remove();
+    });
+  e.stopPropagation();
+});
+
+//add a todo to the list
+$('input[type="text"]').keypress(function(e) {
+  if (e.which === 13) {
+    var todoText = $(this).val();
+    $(this).val('');
+    $('ul').append('<li><span>X</span> ' + todoText + '</li>');
+  }
+});
